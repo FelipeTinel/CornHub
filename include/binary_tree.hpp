4c78288e260996733dfd04a5910268aa370fb618
FelipeTinel <felipemacedotinel@gmail.com>
2026-06-09 08:25:17 -0300

    feat: add binary tree hpp

---
+++ include/binary_tree.hpp
@@ -0,0 +1,23 @@
+#pragma once
+
+#include <iostream>
+#include <string>
+
+#include "node2.hpp"
+
+template <typename T>
+class BinaryTree {
+
+    private:
+
+        Node2<T> * root;
+
+    public:
+    
+        BinaryTree (): root(nullptr) {}
+
+    
+        void insert(T info);
+        void remove (T info);
+
+};
