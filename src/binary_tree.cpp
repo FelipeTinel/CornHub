29cb9546b68fd376c9a9026b653b57775de08534
FelipeTinel <felipemacedotinel@gmail.com>
2026-06-09 08:34:05 -0300

    feat: add methods carcass into binary tree cpp

---
+++ src/binary_tree.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+#include "include/binary_tree.hpp"
+
+template <typename T>
+void BinaryTree<T>::insert(T info) {
+
+
+
+}
+
+template <typename T>
+bool BinaryTree<T>::search(T info) {
+
+    
+
+}
+
+template <typename T>
+void BinaryTree<T>::remove(T info) {
+
+    
+
+}
